f240af164f10016e27cc62635f0d0ffa8e4f2ab5
Rakhesh Sasidharan <me@rakhesh.com>
2020-08-23 10:17:38 +0100

    fixed version typo in buildandpush.sh script

--- buildandpush.sh
+++ buildandpush.sh
@@ -6,6 +6,6 @@ if [[ -z $1 ]]; then
     IMAGE="rakheshster/stubby-unbound"
 fi
 
-BASE_VERSION=3.12-2.0.0.1
+VERSION=v0.2.1
 
-docker buildx build --platform linux/amd64,linux/arm64,linux/386,linux/arm/v7,linux/arm/v6 . --push -t ${IMAGE}:${BASE_VERSION}
+docker buildx build --platform linux/amd64,linux/arm64,linux/386,linux/arm/v7,linux/arm/v6 . --push -t ${IMAGE}:${VERSION}
