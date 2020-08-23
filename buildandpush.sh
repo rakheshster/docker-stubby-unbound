ff73efa335644505d21f8acd5b8c46eaad0cd1f4
Rakhesh Sasidharan <me@rakhesh.com>
2020-08-23 14:44:18 +0400

    Slight changes to build scripts

--- buildandpush.sh
+++ buildandpush.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Usage ./buildandpush.sh [image name]
 
+VERSION=v0.2.1
+
 # if no image name, default to something
 if [[ -z $1 ]]; then
     IMAGE="rakheshster/stubby-unbound"
 fi
-
-VERSION=v0.2.1
 
 docker buildx build --platform linux/amd64,linux/arm64,linux/386,linux/arm/v7,linux/arm/v6 . --push -t ${IMAGE}:${VERSION}
--- buildlocal.sh
+++ buildlocal.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Usage ./buildlocal.sh [image name]
 
+VERSION=v0.2.1
+
 # if no image name, default to something
 if [[ -z $1 ]]; then 
     IMAGE="rakheshster/stubby-unbound"
@@ -9,10 +11,10 @@ fi
 # delete an existing image of the same name if it exists
 # thanks to https://stackoverflow.com/questions/30543409/how-to-check-if-a-docker-image-with-a-specific-tag-exist-locally
 if [[ $(docker image inspect ${IMAGE} 2>/dev/null) == "" ]]; then 
-    docker rmi -f ${IMAGE}
+    docker rmi -f ${IMAGE}:${VERSION}
 fi
 
-docker build . -t ${IMAGE}
+docker build . -t ${IMAGE}:${VERSION}
 
 # prune the intermediate images
 # skip this for now as I want to keep them around to improve build times ...
