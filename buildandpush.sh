c685a2dac7aab997dba6b772a122cce6821d502d
Rakhesh Sasidharan <me@rakhesh.com>
2020-08-24 18:53:40 +0100

    Added progress

--- buildandpush.sh
+++ buildandpush.sh
@@ -8,4 +8,4 @@ if [[ -z $1 ]]; then
     IMAGE="rakheshster/stubby-unbound"
 fi
 
-docker buildx build --platform linux/amd64,linux/arm64,linux/386,linux/arm/v7,linux/arm/v6 . --push -t ${IMAGE}:${VERSION}
+docker buildx build --platform linux/amd64,linux/arm64,linux/386,linux/arm/v7,linux/arm/v6 . --push -t ${IMAGE}:${VERSION} --progress=plain
